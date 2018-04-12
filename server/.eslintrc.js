dcca2f1a1ff6b75b812887e16f725ba9beb11843
Dejan Milicevic <dejan.milicevic.1986@gmail.com>
2018-04-12 10:40:18 +0200

    setup of the express.js server

---
+++ server/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "rules": {
+        "indent": [
+            2,
+            "tab"
+        ],
+        "no-tabs": 0,
+        "quotes": [
+            2,
+            "single"
+        ],
+        "linebreak-style": [
+            2,
+            "unix"
+        ],
+        "semi": [
+            2,
+            "always"
+        ],
+        "no-console": 0
+    },
+    "env": {
+        "node": true
+    },
+    "extends": "standard"
+};
